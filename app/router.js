e66ba5285ee9952a555f9b9d4a48db78032d8306
dakkadb <dakkadakkadakkadb@gmail.com>
2016-05-27 16:20:31 -0400

    faction routes / header
    Finished setting up faction routes, along with some header tweaks

--- app/router.js
+++ app/router.js
@@ -9,7 +9,18 @@ Router.map(function() {
   this.route('myDecks');
   this.route('decklists');
   this.route('cards');
-  this.route('factions');
+  this.route('factions', function() {
+    this.route('astraMilitarum');
+    this.route('spaceMarines');
+    this.route('ork');
+    this.route('chaos');
+    this.route('eldar');
+    this.route('darkEldar');
+    this.route('tau');
+    this.route('tyranids');
+    this.route('necrons');
+  });
+  this.route('register');
 });
 
 export default Router;
---
+++ tests/unit/routes/factions/space-marines-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:factions/space-marines', 'Unit | Route | factions/space marines', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
